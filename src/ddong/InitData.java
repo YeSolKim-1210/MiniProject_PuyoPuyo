27c974c55fa8a3635f9d37afd030c4125378ac22
YeSolKim-1210 <1210yesol@gmail.com>
2020-12-09 13:00:56 +0900

    PUYOPUYO_PROJ
    PUYOPUYO PROJ FILES

---
+++ src/ddong/InitData.java
@@ -0,0 +1,10 @@
+package ddong;
+
+public class InitData {
+
+	public static String ip = "192.168.1.9"; // 발표서버
+//	public static String ip = "192.168.0.127"; // 자습실 서버
+//	public static String ip = "192.168.0.117"; // 자습실 서버
+	public static String id = "hr", pw = "hr";
+
+}
